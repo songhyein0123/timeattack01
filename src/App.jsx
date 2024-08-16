61c0746b7803fa58e275c64f3d412b3e6276ec54
t2023-m0125 <t2023-m0125@t2023-m0125ui-MacBookAir.local>
2024-08-16 13:30:19 +0900

    오타수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-// App.jsx
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [names, setNames] = useState([]);
   const [name, setName] = useState("");
-  const [gold, setGold] = usestate(0);
-  const [silver, setSilver] = usestate(0);
-  const [bronze, setBronze] = usestate(0);
+  const [gold, setGold] = useState(0);
+  const [silver, setSilver] = useState(0);
+  const [bronze, setBronze] = useState(0);
 
   const handleNameChange = (event) => {
-    setName(event.targer.value);
+    setName(event.target.value);
   };
 
   const handleGoldChange = (event) => {
-    setGold(Number(event.targer.value));
+    setGold(Number(event.target.value));
   };
 
   const handleSilverChange = (event) => {
-    setSilver(Number(event.targer.value));
+    setSilver(Number(event.target.value));
   };
 
   const handleBronzeChange = (event) => {
-    setBronze(Number(event.targer.value));
+    setBronze(Number(event.target.value));
   };
 
   const handleAddCountry = (event) => {
@@ -32,7 +31,7 @@ function App() {
       medals: { gold, silver, bronze },
     };
 
-    setNames([...name, newName]);
+    setNames([...names, newName]);
     setName("");
     setGold(0);
     setSilver(0);
@@ -44,27 +43,29 @@ function App() {
       <h1>2024 파리 올림픽 메달 트래커</h1>
       <form className="input-group" onSubmit={handleAddCountry}>
         <p>국가명</p>
-        <input type="text" value={"name"} onChange={handleNameChange} />
+        <input type="text" value={name} onChange={handleNameChange} />
         <p>금메달</p>
-        <input type="Number" value={"gold"} onChange={handleGoldChange} />
+        <input type="number" value={gold} onChange={handleGoldChange} />
         <p>은메달</p>
-        <input type="Number" value={"silver"} onChange={handleSilverChange} />
+        <input type="number" value={silver} onChange={handleSilverChange} />
         <p>동메달</p>
-        <input type="Number" value={"bronze"} onChange={handleBronzeChange} />
+        <input type="number" value={bronze} onChange={handleBronzeChange} />
         <button type="submit">국가추가</button>
         <button>업데이트</button>
       </form>
       <div>
-        (name.length === 0 ? (<p>국가를 추가해주세요.</p>)) : ({" "}
-        <ul>
-          {names.map((name, index) => (
-            <li key={index}>
-              {name.name} : 금메달 {name.medals.gold}, 은메달{" "}
-              {name.medals.silver}, 동메달 {name.medals.bronze}
-            </li>
-          ))}
-        </ul>
-        )
+        {names.length === 0 ? (
+          <p>국가를 추가해주세요.</p>
+        ) : (
+          <ul>
+            {names.map((name, index) => (
+              <li key={index}>
+                {name.name} : 금메달 {name.medals.gold}, 은메달{" "}
+                {name.medals.silver}, 동메달 {name.medals.bronze}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
